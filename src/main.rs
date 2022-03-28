e71df2475afa18af40a994cfe55b246a2955dc05
Lewis Sneddon <>
2022-03-28 16:13:21 +0100

    User input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod card;
 
+use text_io::read;
+
 use card::Card;
 
 fn main() {
-    let example = Card {
-        term: "purchase".to_string(),
-        definition: "buy".to_string(),
-    };
+    let term_prompt = "Enter the term for the card".to_string();
+    let definition_prompt = "Now enter the definition for the card".to_string();
+    println!("{}", term_prompt);
+    let term_entered: String = read!();
+    println!("{}", definition_prompt);
+    let definition_entered: String = read!();
+
+    let example = Card::new(
+        term_entered.to_string(),
+        definition_entered.to_string());
 
     println!("{}", example.to_string());
 }
